c643f7c234956d69b24289fe3ff38b5fc92ae7d8
lw <48504515+liuwei0911@users.noreply.github.com>
2019-11-13 08:43:47 +0800

    Update Image_Negative.cpp

--- Chapter3/Negative_Log_Gamma/Image_Negative.cpp
+++ Chapter3/Negative_Log_Gamma/Image_Negative.cpp
@@ -1,17 +1,18 @@
 #include <opencv2/opencv.hpp>
 #include "Negative_Log_Gamma.hpp"
 
-void Image_Nagetive(){
+//imread constructor
+Negative_Log_Gamma::Negative_Log_Gamma(const std::string& filepath){
+    image = cv::imread(filepath, cv::IMREAD_GRAYSCALE);
+}
 
-    cv::Mat image = cv::imread("D:/image/imageProcessing/DIP3E_Original_Images_CH03/Fig0304(a)(breast_digital_Xray).tif", cv::IMREAD_GRAYSCALE);
-    cv::namedWindow("Window");
+//get the image
+cv::Mat Negative_Log_Gamma::getImage(){
+    return image;
+}
 
-    //show original image
-    cv::displayOverlay("Window", "Original image", 0);
-    cv::imshow("Window", image);
-    cv::waitKey(0);
-
-    //negative
+//negative
+void Negative_Log_Gamma::image_Negative(){
 
     //1.subtract directly
     /*
@@ -29,9 +30,18 @@ void Image_Nagetive(){
     for(cv::MatIterator_<uchar> it = image.begin<uchar>(); it != image.end<uchar>(); ++it){
         *it = 255 - *it;
     }
+}
+//gamma
+void Negative_Log_Gamma::gamma_transformation(double c, double gamma){
+    for(auto it = image.begin<uchar>(); it != image.end<uchar>(); ++it){
+        *it = static_cast<uchar>(c * std::pow(*it, gamma));
+    }
+    cv::normalize(image, image, 0, 255, cv::NORM_MINMAX, -1, cv::noArray());
+}
 
-    //show negative image
-    cv::displayOverlay("Window", "Negative image", 0);
-    cv::imshow("Window", image);
-    cv::waitKey(0);
+//imshow
+void Negative_Log_Gamma::imshow(const std::string& overlay, const std::string& windowName){
+    cv::namedWindow(windowName);
+    cv::displayOverlay(windowName, overlay, 0);
+    cv::imshow(windowName, image);
 }
