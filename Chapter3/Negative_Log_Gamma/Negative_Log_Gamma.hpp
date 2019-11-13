d4008e8e9722e28c9fb68a177e57187eb3aeea74
lw <48504515+liuwei0911@users.noreply.github.com>
2019-11-13 08:44:10 +0800

    Update Negative_Log_Gamma.hpp

--- Chapter3/Negative_Log_Gamma/Negative_Log_Gamma.hpp
+++ Chapter3/Negative_Log_Gamma/Negative_Log_Gamma.hpp
@@ -1,6 +1,27 @@
 #ifndef NEGATIVE_LOG_GAMMA_HPP
 #define NEGATIVE_LOG_GAMMA_HPP
 
-void Image_Nagetive();
+#include <opencv2/opencv.hpp>
+
+#define SCREEN_WIDTH  1920
+#define SCREEN_HEIGHT 980
+
+class Negative_Log_Gamma{
+    public:
+        Negative_Log_Gamma() = default;
+        Negative_Log_Gamma(const std::string& filepath);
+        Negative_Log_Gamma(cv::Mat& img): image(img) {}
+
+        cv::Mat getImage();
+        //negative
+        void image_Negative();
+        //gamma
+        void gamma_transformation(double c, double gamma);
+
+        void imshow(const std::string& overlay, const std::string& windowName);
+  private:
+        cv::Mat image;
+
+};
 
 #endif // NEGATIVE_LOG_GAMMA_HPP
